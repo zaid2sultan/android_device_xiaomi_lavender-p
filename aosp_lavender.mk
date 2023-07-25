f143d7d63c2a49cf15091dad936ff6db197950ac
ImSurajx <SurajKumar18jun2004@gmail.com>
2023-07-25 12:49:07 +0000

    lavender: Added Some Extra Features
    Signed-off-by: ImSurajx <SurajKumar18jun2004@gmail.com>

--- aosp_lavender.mk
+++ aosp_lavender.mk
@@ -10,9 +10,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common PixelExperience stuff
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_INCLUDE_STOCK_ACORE := false
 TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_SUPPORTS_CALL_RECORDING := true
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
