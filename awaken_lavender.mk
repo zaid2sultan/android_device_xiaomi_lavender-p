84608219d2befe2300fd6826d3320626f32e35e7
zaid2sultan <100853058+zaid2sultan@users.noreply.github.com>
2023-09-15 02:44:11 +0530

    Update and rename aosp_lavender.mk to awaken_lavender.mk

--- awaken_lavender.mk
+++ awaken_lavender.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common PixelExperience stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common ProjectAwaken stuff
+$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
@@ -18,7 +18,7 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_SUPPORTS_GOOGLE_RECORDER := false
 TARGET_SUPPORTS_CALL_RECORDING := true
-TARGET_USES_AOSP_RECOVERY := true
+TARGET_USES_AOSP_RECOVERY := false
 
 # Inherit Some Applictions
 $(call inherit-product, packages/apps/android/Camera/config.mk)
@@ -28,7 +28,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 $(call inherit-product, $(LOCAL_PATH)/device-hidl.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_lavender
+PRODUCT_NAME := awaken_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
